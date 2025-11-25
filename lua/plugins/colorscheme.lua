eb568506635e60ff0b19f84d3647def39fdcbd90
Nikita Ljubimov <nikitaljubimov@gmail.com>
2025-11-25 13:41:00 +0100

    hewo

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,9 +2,14 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-      vim.cmd.colorscheme("tokyonight-storm")
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  end,
+--  config = function()
+--      local everforest = require("everforest-nvim")
+--      everforest.setup({
+--          background = "hard",
+--      })
+--      vim.cmd.colorscheme("everforest")
+--      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--      everforest.load()
+--  end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-lualine/lualine.nvim',
     options = {
-        theme = 'palenight',
+        theme = 'everforest',
     },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
